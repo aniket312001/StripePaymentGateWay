f3c266cd6469816100161ac434436533fb59f959
aniket <aniketchavan505@gmail.com>
2023-01-27 09:39:37 +0530

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ app.use(cors())
 const stripe = require('stripe')('sk_test_51MTLclSEGy4UZS1BDSr8EwvIucpDgeKek40cJBpJ0R1AxkuFR8pP0hp38ccF6hbrvrb0L8b1gWkx1asoAuFycHcS00fdDXcnXq');
 
 
-app.get('/getURL', getSession)
-
-
-
+app.post('/', getSession)
 
 
 async function  getSession(req, res) {
@@ -20,25 +17,25 @@ async function  getSession(req, res) {
 		  
 		  price_data: {
 			currency: 'usd',
-			unit_amount: 2000,
+			unit_amount: req.body.unit_amount,
 			product_data: {
-			  name: 'T-shirt',
-			  description: 'Comfortable cotton t-shirt',
+			  name: req.body.name,
+			  description: req.body.description,
 			},
 		  },
 		  adjustable_quantity: {enabled:false},
-		  quantity: 1,
+		  quantity: req.body.quantity,
 		  
 		}],
 		mode: 'payment',
-		success_url: 'http://localhost:4200?success=true',
-		cancel_url: 'http://localhost:4200/',
+		success_url: req.body.success_url,
+		cancel_url: req.body.cancel_url,
 		invoice_creation: {enabled: true},
 		
 	  });
 	  
 	  res.json(session.url)
-	  console.log(session)
+
 }
 
 
